e22c614b75a2b70ce68b2403a20f29edc7450775
aaronalba <aaalba1@up.edu.ph>
2021-12-08 20:11:50 +0800

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,67 +1,89 @@
 import * as React from 'react';
-import { useState } from 'react';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { 
+  AppBar, 
+  Drawer, 
+  IconButton, 
+  Toolbar, 
+  Typography, 
+  Box, 
+  List, 
+  ListItem, 
+  ListItemIcon, 
+  ListItemText } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import SubjectIcon from '@mui/icons-material/Subject';
+import InfoIcon from '@mui/icons-material/Info';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Colors } from '../utils/Colors';
 
+const drawerWidth = 200
+
 export default function Header() {
-  const [homeHover, setHomeHover] = useState(false)
-  const [aboutHover, setAboutHover] = useState(false)
-  const [writeUpHover, setWriteUpHover] = useState(false)
+  const [isDrawerOpen, setDrawerOpen] = useState(false)
+
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const drawerItems = [
+    {text: "Home", icon: <SubjectIcon/>, path: "/"},
+    {text: "About", icon: <InfoIcon />, path: "/about"},
+  ]
+
+  /**
+   * Shows and hides the drawer
+   */
+  const toggleDrawer = () => setDrawerOpen(!isDrawerOpen)
 
   return (
-    <Box sx={{ flexGrow: 1, position: "absolute", }}>
-      <Toolbar
-        sx={{borderBottom: 1, borderColor: "divider"}}>
-            <Typography
-                variant="h6"
-                component="h6">
-                Math 10 Project
-            </Typography>
+    <div>
+      <AppBar>
+        <Toolbar>
+          {/* Menu */}
+          <IconButton
+            onClick={toggleDrawer}
+            edge="start"
+            color="inherit"
+            aria-label="menu">
+            <MenuIcon/>
+          </IconButton>
 
-            <div style={{
-              position: 'absolute',
-              right: 0,
-              marginRight: 50
-            }}>
-              <Link to="/"              
-                onMouseOver={()=>setHomeHover(true)}
-                onMouseOut={()=>setHomeHover(false)}
-                style={homeHover ? hoveredLinkStyle : linkStyle}>
-                Home
-              </Link>
-
-              <Link to="/" 
-                onMouseOver={()=>setWriteUpHover(true)}
-                onMouseOut={()=>setWriteUpHover(false)}
-                style={writeUpHover ? hoveredLinkStyle : linkStyle}>
-                Write-up
-              </Link>
-
-              <Link to="/about"
-                onMouseOver={()=>setAboutHover(true)}
-                onMouseOut={()=>setAboutHover(false)}
-                style={aboutHover ? hoveredLinkStyle : linkStyle}>
-                About
-              </Link>
-            </div>
+          {/* Title */}
+          <Typography
+            sx={{
+              flexGrow: 1,
+              textAlign: "center"
+            }}
+            variant="h6">
+            Math 10 Project
+          </Typography>
         </Toolbar>
-    </Box>
+      </AppBar>
+
+      {/* Side Drawer */}
+      <Drawer
+        variant="temporary"
+        open={isDrawerOpen}
+        onClose={toggleDrawer}
+        anchor="left">
+          {/* List of Drawer Buttons */}
+          <Box
+            width={drawerWidth}>
+            <List>
+              {drawerItems.map(item => (
+                // Individual Button
+                <ListItem
+                  sx={{backgroundColor: (location.pathname == item.path) ? Colors.lightGray : null}}
+                  onClick={() => navigate(item.path)}
+                  button
+                  key={item.text}>
+                  <ListItemIcon>{item.icon}</ListItemIcon>
+                  <ListItemText primary={item.text} />
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+      </Drawer>
+    </div>
   );
-}
-
-const linkStyle = {
-  textDecoration: "none",
-  marginLeft: 50,
-  color: Colors.primary,
-  padding:5,
-  // borderRadius: 10,
-  fontSize: 18,
-}
-
-const hoveredLinkStyle = {
-  ...linkStyle,
-  borderBottom: `1px solid ${Colors.primary}`
 }
