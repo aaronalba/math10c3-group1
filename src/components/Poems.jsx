8719104262e4ce400910a6bcdc9559c214e0f90a
aaronalba <aaalba1@up.edu.ph>
2021-12-09 11:44:12 +0800

    add poem images

--- src/components/Poems.jsx
+++ src/components/Poems.jsx
@@ -3,6 +3,9 @@ import { Box } from '@mui/system';
 import * as React from 'react';
 import { Font } from '../utils/Font';
 import Sample from '../img/sample.png';
+import PuzzlingWorld from '../img/puzzling_world.png';
+import OneTwist1 from '../img/one_twist_1.png';
+import OneTwist2 from '../img/one_twist_2.png';
 
 export default function Poems() {
     return (
@@ -22,13 +25,29 @@ export default function Poems() {
                 align="center"
                 padding={10}>
                 {/* Poem 1 */}
-                <Grid item xs={12} md={6}>
-                    <img src={Sample} alt="poem" />
+                <Grid item xs={12} md={12}>
+                    <img
+                        width={480}
+                        height={679}
+                        src={PuzzlingWorld} 
+                        alt="poem" />
                 </Grid>
 
-                {/* Poem 2 */}
+                {/* Poem 2 Part 1*/}
                 <Grid item xs={12} md={6}>
-                    <img src={Sample} alt="poem" />
+                    <img
+                        width={480}
+                        height={853} 
+                        src={OneTwist1}
+                        alt="poem" />
+                </Grid>
+                {/* Poem 2 Part 2*/}
+                <Grid item xs={12} md={6}>
+                    <img
+                        width={480}
+                        height={853} 
+                        src={OneTwist2}
+                        alt="poem" />
                 </Grid>
             </Grid>
         </Box>
